d9b88accc8e2b438f8e879bac822881d21418863
thedancollins <thedancollins@hotmail.com>
2019-02-02 16:44:13 -0600

    drop table values if exists

--- server/index.js
+++ server/index.js
@@ -21,11 +21,11 @@ const pgClient = new Pool({
 pgClient.on('error', () => console.log('Lost PG connection'));
 
 pgClient
-  .query('CREATE TABLE IF NOT EXISTS values (number INT)')
-  .catch(err => console.log(err));
+.query('DROP TABLE IF EXISTS values;')
+.catch(err => console.log(err));
 
-  pgClient
-  .query('delete from values')
+pgClient
+  .query('CREATE TABLE IF NOT EXISTS values (number INT)')
   .catch(err => console.log(err));
 
 // Redis Client Setup
